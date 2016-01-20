deaa5fcb2fd07752366dead17a032a294d9334e2
venkatkrishnan <venkatkrishnan.u@hotmail.com>
2016-01-20 18:38:24 +0530

    custom directives

--- public/javascript/controller.js
+++ public/javascript/controller.js
@@ -6,3 +6,75 @@ visualApp.config(['$routeProvider', function($routeProvider) {
       templateUrl: 'pages/home.html'
     });
 }]);
+
+visualApp.directive("tabsData", function($rootScope, $http) {
+  return {
+    restrict : 'E',
+    templateUrl : 'pages/tabs.html',
+    controller: function() {
+      $http.get('/tabs').success(function(data) {
+        $rootScope.dashboards = data.dashboards;
+        $rootScope.currentTab = data.dashboards[0].tabId;
+
+        $rootScope.onClickTab = function (tab) {
+            $rootScope.currentTab = tab.tabId;
+        }
+
+        $rootScope.isActiveTab = function(tabId) {
+            return tabId == $rootScope.currentTab;
+        }
+
+        $rootScope.username = data.name;
+      });
+    }
+  };
+});
+
+visualApp.directive("dashboardData", function($rootScope, $http, $log, ChartService) {
+  return {
+    restrict : 'E',
+    templateUrl : 'pages/dashboard.html',
+    controller: function() {
+
+      $http.get('/dashboards').success(function(json) {
+        $rootScope.dashboardItems = json;
+        $rootScope.currentTab = json[0].tabId;
+
+        $rootScope.onClickTab = function (tab) {
+            $rootScope.currentTab = tab.tabId;
+        }
+
+        $rootScope.isActiveTab = function(tabId) {
+            return tabId == $rootScope.currentTab;
+        }
+      });
+
+      $http.get('chartData/widgets').success(function(widgets) {
+        $rootScope.widgetItems = widgets;
+
+        $rootScope.widget = function(widgetId, widgetContainer) {
+          widgets.forEach(function(w) {
+             if(w.widgetId === widgetId) {
+                $rootScope.title = w.title;
+                $rootScope.chartRenderer = w.chartRenderer;
+                $rootScope.url = w.url;
+                $rootScope.comments = w.comments;
+
+                var containerWidth = $("dashboardData").clientWidth;
+                $log.log(containerWidth);
+                // var widgetWidth = $("#"+widgetContainer).width();
+                //
+                // if(widgetWidth > 100) {
+                //   var width = $("#"+widgetContainer).width();
+                //   var parentWidth = $("#" + widgetContainer).offsetParent().width();
+                //   widgetWidth = (width * 100)/parentWidth;
+                // }
+                // var chartWidth = (containerWidth * widgetWidth)/100;
+                // ChartService[$rootScope.chartRenderer]("#" + widgetContainer, chartWidth, $rootScope.url);
+             }
+          })
+        }
+      });
+    }
+  };
+});
--- public/javascript/home.js
+++ public/javascript/home.js
@@ -1,48 +1,4 @@
 angular.module('visualApp').controller('homeController', function($scope, $http, $log, ChartService) {
 
-  $http.get('/tabs').success(function(data) {
-    $scope.dashboards = data.dashboards;
-    $scope.currentTab = data.dashboards[0].tabId;
-
-    $scope.onClickTab = function (tab) {
-        $scope.currentTab = tab.tabId;
-    }
-
-    $scope.isActiveTab = function(tabId) {
-        return tabId == $scope.currentTab;
-    }
-
-    $scope.username = data.name;
-  });
-
-  $http.get('/dashboards').success(function(json) {
-    $scope.dashboardItems = json;
-    $scope.currentTab = json[0].tabId;
-
-    $scope.onClickTab = function (tab) {
-        $scope.currentTab = tab.tabId;
-    }
-
-    $scope.isActiveTab = function(tabId) {
-        return tabId == $scope.currentTab;
-    }
-  });
-
-  $http.get('chartData/widgets').success(function(widgets) {
-    $scope.widgetItems = widgets;
-
-    $scope.widget = function(widgetId, widgetContainer) {
-      widgets.forEach(function(w) {
-         if(w.widgetId === widgetId) {
-            $scope.title = w.title;
-            $scope.chartRenderer = w.chartRenderer;
-            $scope.url = w.url;
-            $scope.comments = w.comments;
-
-            ChartService[$scope.chartRenderer]("#" + widgetContainer, 500, $scope.url);
-
-         }
-      })
-    }
-  });
+  
 });
