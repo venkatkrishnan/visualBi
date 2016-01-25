c12849c8a6c207fb5f69b1be9170adb15ea73d68
NabilaRafi <nabila.r76@wipro.com>
2016-01-25 12:28:12 +0530

    chart issue fixed

--- public/javascript/directive.js
+++ public/javascript/directive.js
@@ -30,9 +30,11 @@ visualApp.directive("headData", function() {
   return {
     restrict : 'E',
     templateUrl : 'pages/dashboard.html',
-    link: function($scope, $element, $attr) {
-
-      $http.get('/dashboards').success(function(json) {
+    scope: {},
+    link: function($scope, $element, $attr,scope) {
+      $log.log("Calling link inside the dashboardData");
+      var dashboardPromise = new Promise(function(resolve,reject) {
+        $http.get('/dashboards').success(function(json) {
         $scope.dashboardItems = json;
         $scope.currentTab = json[0].tabId;
 
@@ -43,53 +45,56 @@ visualApp.directive("headData", function() {
         $scope.isActiveTab = function(tabId) {
             return tabId == $scope.currentTab;
         }
+        resolve("success");
       });
-
-      $http.get('chartData/widgets').success(function(widgets) {
+    });
+    var widgetPromise = new Promise(function(resolve,reject) {
+        $http.get('chartData/widgets').success(function(widgets) {
         $scope.widgetItems = widgets;
 
-        $scope.widget = function(widgetId) {
-
-          widgets.forEach(function(w) {
-             if(w.widgetId === widgetId) {
-                $scope.title = w.title;
-                $scope.chartRenderer = w.chartRenderer;
-                $scope.url = w.url;
-                $scope.comments = w.comments;
-             }
-          })
-        }
+        // scope.widget = function(widgetId) {
+        //   widgets.forEach(function(w) {
+        //      if(w.widgetId === widgetId) {
+        //         $scope.title = w.title;
+        //         $scope.chartRenderer = w.chartRenderer;
+        //         $scope.url = w.url;
+        //         $scope.comments = w.comments;
+        //      }
+        //   });
+        // }
+        resolve("success");
       });
-    }
-  };
+    });
+
+    Promise.all([dashboardPromise,widgetPromise]).then(function() {
+      $scope.dashboardItems.map(function(dashboardItem) {
+        dashboardItem.rows.forEach(function(row) {
+          $scope.widgetItems.forEach(function(widgetItem) {
+            if(row.widgetId == widgetItem.widgetId) {
+              row.title = widgetItem.title;
+              row.chartRenderer = widgetItem.chartRenderer;
+              row.url = widgetItem.url;
+              row.comments = widgetItem.comments;
+            }
+          });
+        });
+      });
+    });
+  }};
 }).directive("barChart", ['$log', 'plotContinentChart', 'gdpPerCapitaBarChart', 'gdpStackedBarChart', 'plotNorthEast', function($log,
                                                                     plotContinentChart, gdpPerCapitaBarChart, gdpStackedBarChart, plotNorthEast) {
   return {
     restrict : 'E',
     templateUrl : 'pages/barchart.html',
     replace: true,
+    transclude: true,
     scope: {
       parameters : "@",
-      chartRenderer : "@",
-      tabs : "="
+      chartRenderer : "@"
     },
     link: function(scope, elements, attrs) {
-
-      // if(parseInt(elements[0].clientWidth) === 0) {
-      //   var myWatcher = scope.$watch(function() {
-  	  //     return elements[0].clientWidth;
-      //   }, function(value){
-	    //     if(value > 0) {
-      //       var chartMethod = scope.chartRenderer + ".render" + '(elements[0]' +
-      //                           ', ' + value + ', "' + scope.parameters + '")';
-      //
-      //       eval(chartMethod).then(function(data) {
-      //         myWatcher();
-      //       });
-      //     }
-      //   });
-      // } else {
-
+      $log.log("calling link iside the barChart");
+      $log.log(scope.chartRenderer);
       var chartMethod = scope.chartRenderer + ".render" + '(elements[0]' +
                         ', ' + elements[0].clientWidth + ', "' + scope.parameters + '")';
       eval(chartMethod);
